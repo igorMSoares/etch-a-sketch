0c58a85be0fcd3a64efca9c41044dec793a97933
IguhSoares <igorms.dev@gmail.com>
2023-01-14 06:23:43 -0300

    fix: invalid path for dynamically loaded html2canvas.js

--- public/js/script.js
+++ public/js/script.js
@@ -615,7 +615,10 @@ const initDownloadHandler = () => {
     const isLoaded = document.getElementById(HTML2C_SCRIPT_ID) ? true : false;
 
     if (!isLoaded) {
-      await loadScript('./public/js/html2canvas.min.js', HTML2C_SCRIPT_ID);
+      await loadScript(
+        `${window.location}/public/js/html2canvas.min.js`,
+        HTML2C_SCRIPT_ID
+      );
     }
 
     html2canvas(canvas, {

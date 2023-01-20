5d71f38eb011d1e9a316c252aa615ba9c137908f
IguhSoares <igorms.dev@gmail.com>
2023-01-20 14:44:24 -0300

    refactor: observers to init canvas and to init download handler
    also refactors download-canvas.js methods

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,81 @@
 import isMobile from './is-mobile.js';
 
-const lazyRenderCanvas = (opts = {}) => {
+const initObserver = (observed_element_id, callback) => {
+  const observer = new IntersectionObserver(async (entries, thisObserver) => {
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        if (callback instanceof Function) callback();
+
+        thisObserver.unobserve(document.getElementById(observed_element_id));
+      }
+    }
+  });
+
+  observer.observe(document.getElementById(observed_element_id));
+};
+
+const initCanvasObserver = (opts = {}) => {
   const {
-    totalColumns: TOTAL_COLUMNS = 30,
-    loading: LOADING = {
+    totalColumns = 30,
+    loading = {
       className: 'loading-canvas',
       message: 'Loading Canvas...',
       messageAreaId: 'color-picker',
     },
-    observedElementId: OBSERVED_ELEMENT_ID = 'reset-canvas',
-    complete = false,
+    observedElementId = 'reset-canvas',
   } = opts;
 
   const loadingMsg = (() => {
     /** Container must always be a <p>
      * If changed to different element, must be changed in renderCanvas() too */
     const p = document.createElement('p');
-    p.classList.add(LOADING.className);
-    p.innerHTML = LOADING.message;
+    p.classList.add(loading.className);
+    p.innerHTML = loading.message;
     return p;
   })();
 
-  const observer = new IntersectionObserver(async (entries, thisObserver) => {
-    for (const entry of entries) {
-      if (entry.isIntersecting) {
-        document.getElementById(LOADING.messageAreaId).appendChild(loadingMsg);
-        const { renderCanvas } = await import('./canvas.js');
-        renderCanvas(TOTAL_COLUMNS);
-        const { initChangeColorHandler } = await import('./color-picker.js');
-        initChangeColorHandler();
+  initObserver(observedElementId, async () => {
+    document.getElementById(loading.messageAreaId).appendChild(loadingMsg);
+    const { renderCanvas } = await import('./canvas.js');
+    renderCanvas(totalColumns);
 
-        thisObserver.unobserve(document.getElementById(OBSERVED_ELEMENT_ID));
+    const { initChangeColorHandler } = await import('./color-picker.js');
+    initChangeColorHandler();
 
-        if (complete instanceof Function) complete();
-      }
+    const { Brush } = await import('./brush.js');
+    const { resizeCanvas, initResetCanvasHandlers } = await import(
+      './canvas.js'
+    );
+    if (!isMobile()) {
+      document.getElementById('canvas').addEventListener('click', () => {
+        Brush.state = Brush.isOn ? 'off' : 'on';
+      });
+      window.onresize = resizeCanvas;
+    } else {
+      Brush.state = 'on';
+      screen.orientation.addEventListener('change', resizeCanvas);
     }
-  });
 
-  observer.observe(document.getElementById(OBSERVED_ELEMENT_ID));
+    const { initColorModeHandler } = await import('./color-modes.js');
+    [
+      'erase-mode',
+      'random-color-mode',
+      'progressive-darkening-mode',
+      'progressive-lighten-mode',
+      'toggle-grid',
+    ].forEach(mode => initColorModeHandler(mode));
+
+    initResetCanvasHandlers();
+  });
+};
+
+const initDownloadCanvasObserver = (observedElementId = 'download') => {
+  initObserver(observedElementId, async () => {
+    const { default: initDownloadHandler } = await import(
+      './download-canvas.js'
+    );
+    initDownloadHandler();
+  });
 };
 
 const start = async () => {
@@ -45,42 +83,8 @@ const start = async () => {
     './toggle-instructions.js'
   );
   initToggleInstructionsHandler();
-
-  addEventListener('load', async _ => {
-    const { default: lazyDownloadCanvasHandler } = await import(
-      './download-canvas.js'
-    );
-    lazyDownloadCanvasHandler();
-  });
-
-  lazyRenderCanvas({
-    complete: async () => {
-      const { Brush } = await import('./brush.js');
-      const { resizeCanvas, initResetCanvasHandlers } = await import(
-        './canvas.js'
-      );
-      if (!isMobile()) {
-        document.getElementById('canvas').addEventListener('click', () => {
-          Brush.state = Brush.isOn ? 'off' : 'on';
-        });
-        window.onresize = resizeCanvas;
-      } else {
-        Brush.state = 'on';
-        screen.orientation.addEventListener('change', resizeCanvas);
-      }
-
-      const { initColorModeHandler } = await import('./color-modes.js');
-      [
-        'erase-mode',
-        'random-color-mode',
-        'progressive-darkening-mode',
-        'progressive-lighten-mode',
-        'toggle-grid',
-      ].forEach(mode => initColorModeHandler(mode));
-
-      initResetCanvasHandlers();
-    },
-  });
+  initCanvasObserver();
+  initDownloadCanvasObserver();
 };
 
 start();
